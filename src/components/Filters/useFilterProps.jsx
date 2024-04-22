4f0e0e98e95037cf2dc392489fbdd71b1fb87501
sanedidi <sanedidi98@gmail.com>
2024-04-22 13:07:09 +0300

    fix: useFilterProps.jsx links

--- src/components/Filters/useFilterProps.jsx
+++ src/components/Filters/useFilterProps.jsx
@@ -14,13 +14,13 @@ export const useFilterProps = () => {
         const formattedData = response.data.map((item) => ({
           id: item.id,
           desc: item.name_ru,
-          img: "https://telegra.ph/file/a61d16c431391e56ae892.png",
+          img: "https://telegra.ph/file/1cceb1e3ad50d76e3c823.png",
         }));
 
         const additionalImageUrls = {
-          2: "https://telegra.ph/file/5d0ab79339c1e60833aa2.png",
-          3: "https://telegra.ph/file/825c38cdf15ec4ba077f1.png",
-          4: "https://telegra.ph/file/04882b66da5d2d0f0d890.png",
+          2: "https://telegra.ph/file/8516aadf3af389ffb4f31.png",
+          3: "https://telegra.ph/file/04882b66da5d2d0f0d890.png",
+          4: "https://telegra.ph/file/a61d16c431391e56ae892.png",
         };
 
         formattedData.forEach((item) => {
